6fb89bab6596f34ee10505968d89efcc25d29021
Juan José Liñán <jjlife06@gmail.com>
2020-03-24 17:40:31 +0100

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/info-equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +11,34 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
-  cargada = false;
+  cargadaInfo = false;
+  equipo: any[] = [];
+  cargadoEquipo = false;
 
   constructor(private http: HttpClient) {
-
-      //console.log('Esto es un servicio');
-      //Leer archivo json
-
-      this.http.get('assets/data/data-pagina.json').subscribe(
-        (resp: InfoPagina) => {
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-         // console.log( resp['twitter'] ); //funciona a pesar del warning
-        }
-      );
-
+     this.cargarInfo();
+     this.cargarEquipo();
   }
 
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json').subscribe(
+      (resp: InfoPagina) => {
+        this.cargadaInfo = true;
+        this.info = resp;
+       // console.log( resp['twitter'] ); //funciona a pesar del warning
+      }
+    );
+ }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-a7f56.firebaseio.com/equipo.json').subscribe(
+      (resp: any[]) => {
+        this.cargadoEquipo = true;
+        this.equipo = resp;
+       // console.log(resp);
+       // console.log( resp['twitter'] ); //funciona a pesar del warning
+      }
+    );
+
+  }
 }
